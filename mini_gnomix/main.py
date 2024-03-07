e422a5b6d32e399942c352ef01fe45d0c8db74c2
Ehsan Hajyasini <esihaj@gmail.com>
2024-03-07 12:22:41 -0800

    bug fix

--- mini_gnomix/main.py
+++ mini_gnomix/main.py
@@ -16,6 +16,11 @@ def greet(name: str = typer.Argument(None, help="Your name")):
     greeting = f"Hello, {name}" if name else "Hello, World!"
     typer.echo(greeting)
 
+def print_green(text):
+    print("\033[92m{}\033[00m" .format(text))
+
+def print_red(text):
+    print("\033[91m{}\033[00m" .format(text))
     
 @app.command()
 def simulate_data(data_path: Annotated[str, typer.Argument()] = "./demo/data/",
@@ -24,7 +29,7 @@ def simulate_data(data_path: Annotated[str, typer.Argument()] = "./demo/data/",
          reference_file: Annotated[str, typer.Argument()] = "reference_1000g.vcf",
          sample_map_file: Annotated[str, typer.Argument()] = "1000g.smap",
          chm: Annotated[str, typer.Argument()] = "22",
-         phase: Annotated[bool, typer.Option(False)] = False,
+         phase: Annotated[bool, typer.Option("--phased", help="Use phased data")] = False,
          output_basename: Annotated[str, typer.Argument()] = "./demo/output"):
     """Simulated Admixed Training Data Using Gnomix"""
     print(f"Current working directory: {os.getcwd()}")
@@ -36,19 +41,36 @@ def simulate_data(data_path: Annotated[str, typer.Argument()] = "./demo/data/",
     sample_map_file = os.path.join(data_path, sample_map_file)
 
     # Generate reference file
-    sample_map = pd.read_csv(sample_map_file, sep="\t")
-    samples = list(sample_map["#Sample"])
-    sample_file = os.path.join(data_path, "samples_1000g.tsv")
-    np.savetxt(sample_file, samples, delimiter="\t", fmt="%s")
-    subset_cmd = f"bcftools view -S {sample_file} -o {reference_file} {query_file}"
-    print(f"Running in command line: \n\t{subset_cmd}")
-    os.system(subset_cmd)
+    #check if reference file exists
+    if not os.path.exists(reference_file):
+        print_green("Generating reference file...")
+        print_red("This will take some time!")
+        sample_map = pd.read_csv(sample_map_file, sep="\t")
+        samples = list(sample_map["#Sample"])
+        sample_file = os.path.join(data_path, "samples_1000g.tsv")
+        np.savetxt(sample_file, samples, delimiter="\t", fmt="%s")
+        subset_cmd = f"bcftools view -S {sample_file} -o {reference_file} {query_file}"
+        print(f"Running in command line: \n\t{subset_cmd}")
+        exit_code = os.system(subset_cmd)
+        if exit_code != 0:
+            print_red("Error running subset command. Exiting...")
+            sys.exit(1)
+        print_green("Reference file generated successfully!")
+    else:
+        print_green("Reference file already exists.")
+    
 
     # Simulate the data and train the model
-    train_cmd = f"python3 external/gnomix/gnomix.py {query_file} {output_basename} {chm} {'True' if phase else 'False'} {genetic_map_file} {reference_file} {sample_map_file} > ./demo/training_log.txt"
-    print("Remember that you just need to wait until simulation data is done!You don't need to wait for the training process")
+    print_green("Simulating data and training the model...")
+    train_cmd = f"python3 external/gnomix/gnomix.py {query_file} {output_basename} {chm} {'True' if phase else 'False'} {genetic_map_file} {reference_file} {sample_map_file}"
+    # > ./demo/training_log.txt
+    print_red("Remember that you just need to wait until simulation data is done! You don't need to wait for the training process")
+    print_red("This will take some time!")
     print(f"Running in command line: \n\t{train_cmd}")
-    os.system(train_cmd)
+    exit_code = os.system(train_cmd)
+    if exit_code != 0:
+        print("Error running train command. Exiting...")
+        sys.exit(1)
     
     
     
